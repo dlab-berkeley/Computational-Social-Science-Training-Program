9b82e6ec2c728f05add14dda3bf618ff86073d44
Aniket Kesari <Aniket.Kesari@gmail.com>
2020-12-04 12:53:25 -0800

    project 4

--- Unsupervised Machine Learning and TPOT/TPOT/tpot_bike_pipeline.py
+++ Unsupervised Machine Learning and TPOT/TPOT/tpot_bike_pipeline.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.8662980729421823
-exported_pipeline = RandomForestRegressor(bootstrap=False, max_features=0.7500000000000001, min_samples_leaf=3, min_samples_split=12, n_estimators=100)
+# Average CV score on the training set was: 0.8426240122418044
+exported_pipeline = make_pipeline(
+    MinMaxScaler(),
+    RandomForestRegressor(bootstrap=True, max_features=0.8, min_samples_leaf=5, min_samples_split=5, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
--- Unsupervised Machine Learning and TPOT/TPOT/tpot_census_pipeline.py
+++ Unsupervised Machine Learning and TPOT/TPOT/tpot_census_pipeline.py
@@ -2,10 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import Normalizer
-from sklearn.tree import DecisionTreeClassifier
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
 from xgboost import XGBClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,10 +11,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.8056101556101556
+# Average CV score on the training set was: 0.7581900081900081
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=DecisionTreeClassifier(criterion="gini", max_depth=1, min_samples_leaf=11, min_samples_split=6)),
-    Normalizer(norm="l1"),
     RBFSampler(gamma=0.8500000000000001),
     XGBClassifier(learning_rate=0.01, max_depth=7, min_child_weight=11, n_estimators=100, nthread=1, subsample=0.1)
 )
