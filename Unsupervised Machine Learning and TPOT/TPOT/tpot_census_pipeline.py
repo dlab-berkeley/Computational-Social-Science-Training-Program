a7dbbdb6fc366cb13065d86650170672a92764a0
sociologyquinn <kellyquinn@berkeley.edu>
2021-11-28 14:17:11 -0800

    update libraries and installation instructions

--- Unsupervised Machine Learning and TPOT/TPOT/tpot_census_pipeline.py
+++ Unsupervised Machine Learning and TPOT/TPOT/tpot_census_pipeline.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.kernel_approximation import RBFSampler
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MaxAbsScaler, Normalizer
 from xgboost import XGBClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -11,10 +11,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.7581900081900081
+# Average CV score on the training set was: 0.8483210483210485
 exported_pipeline = make_pipeline(
-    RBFSampler(gamma=0.8500000000000001),
-    XGBClassifier(learning_rate=0.01, max_depth=7, min_child_weight=11, n_estimators=100, nthread=1, subsample=0.1)
+    MaxAbsScaler(),
+    Normalizer(norm="l2"),
+    XGBClassifier(learning_rate=0.01, max_depth=7, min_child_weight=11, n_estimators=100, n_jobs=1, subsample=0.1, verbosity=0)
 )
 
 exported_pipeline.fit(training_features, training_target)
