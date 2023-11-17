add1731b4951800d083fa18b1a880a84f751cb62
kaseyzapatka <kaseyzapatka@gmail.com>
2023-11-16 16:15:07 -0800

    wip: first pass at annotating script

--- 4 Unsupervised Machine Learning and TPOT/4-2 TPOT/tpot_census_pipeline_new_params.py
+++ 4 Unsupervised Machine Learning and TPOT/4-2 TPOT/tpot_census_pipeline_new_params.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.tree import DecisionTreeClassifier
-from tpot.builtins import StackingEstimator, ZeroCount
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,12 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.6772509641236475
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=DecisionTreeClassifier(criterion="gini", max_depth=8, min_samples_leaf=16, min_samples_split=14)),
-    ZeroCount(),
-    DecisionTreeClassifier(criterion="entropy", max_depth=4, min_samples_leaf=17, min_samples_split=18)
-)
+# Average CV score on the training set was: 0.7167156256893756
+exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=6, max_features=0.8500000000000001, min_samples_leaf=2, min_samples_split=15, n_estimators=100, subsample=0.8500000000000001)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
